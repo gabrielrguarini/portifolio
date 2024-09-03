b3abf84afffbe01b5f3eefe0777538f7f706c318
Gabriel Guarini <gabrielrguarini@gmail.com>
2024-09-03 18:16:47 -0300

    feat: add background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,35 +17,43 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between scroll-smooth">
-        <section
-          className="mt-32 flex h-screen flex-col p-4"
-          data-aos="zoom-out-down"
-          data-aos-duration="500"
+      <main className="flex min-h-screen flex-col items-center justify-between overflow-x-hidden scroll-smooth">
+        <div
+          className="w-screen"
+          style={{
+            backgroundImage: `url("/background.jpg")`,
+            backgroundSize: "cover",
+          }}
         >
-          <div className="mx-auto flex flex-col items-center gap-4 whitespace-nowrap text-4xl lg:flex-row lg:text-6xl">
-            <p className="select-none font-bold">OI, EU SOU</p>
-            <h1
-              data-aos="fade-left"
-              data-aos-delay="500"
-              data-aos-duration="1000"
-              className="rounded-full bg-black p-4 font-extrabold text-white"
-            >
-              GABRIEL GUARINI
-            </h1>
-          </div>
-          <p
-            data-aos="fade-up"
-            data-aos-delay="1000"
-            data-aos-duration="1000"
-            className="mx-auto mt-24 max-w-3xl text-center text-xl text-gray-700"
+          <section
+            className="flex h-screen flex-col p-4"
+            data-aos="zoom-out-down"
+            data-aos-duration="500"
           >
-            Um portifólio mostrando um pouco sobre mim e meus projetos.
-          </p>
-          <Link className="m-auto animate-bounce" href="#SOBRE">
-            <ChevronDown size={100} />
-          </Link>
-        </section>
+            <div className="mx-auto mt-32 flex flex-col items-center gap-4 whitespace-nowrap text-4xl lg:flex-row lg:text-6xl">
+              <p className="select-none font-bold">OI, EU SOU</p>
+              <h1
+                data-aos="fade-left"
+                data-aos-delay="500"
+                data-aos-duration="1000"
+                className="rounded-full bg-black p-4 font-extrabold text-white"
+              >
+                GABRIEL GUARINI
+              </h1>
+            </div>
+            <p
+              data-aos="fade-up"
+              data-aos-delay="1000"
+              data-aos-duration="1000"
+              className="mx-auto mt-24 max-w-3xl text-center text-xl text-gray-700"
+            >
+              Um portifólio mostrando um pouco sobre mim e meus projetos.
+            </p>
+            <Link className="m-auto animate-bounce" href="#SOBRE">
+              <ChevronDown size={100} />
+            </Link>
+          </section>
+        </div>
         <About />
         <Projects />
         <Contact />
